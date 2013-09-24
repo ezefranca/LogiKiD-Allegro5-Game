f4fde9a2a3bf3d012cd88d2818fec2174b6d2bef
talespadua <talescpadua@gmail.com>
2013-09-24 02:50:56 -0300

    Menu mais acima

--- bin/ItensMenu.d
+++ bin/ItensMenu.d
@@ -1,5 +1,5 @@
-/home/ezequiel/Gitcode/ProjetoPI2/bin/ItensMenu.o: \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/ItensMenu.c \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/Player.h \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/comum.h \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/ItensMenu.h
+/home/tales/ProjetoPI2/bin/ItensMenu.o: \
+ /home/tales/ProjetoPI2/UsingMake/src/ItensMenu.c \
+ /home/tales/ProjetoPI2/UsingMake/src/Player.h \
+ /home/tales/ProjetoPI2/UsingMake/src/comum.h \
+ /home/tales/ProjetoPI2/UsingMake/src/ItensMenu.h
--- bin/fase1.d
+++ bin/fase1.d
@@ -1,6 +1,6 @@
-/home/ezequiel/Gitcode/ProjetoPI2/bin/fase1.o: \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/Fases/Fase1/fase1.c \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/Fases/Fase1/fase1.h \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/Fases/Fase1/../../comum.h \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/Fases/Fase1/../../Player.h \
- /home/ezequiel/Gitcode/ProjetoPI2/UsingMake/src/Fases/Fase1/../../ItensMenu.h
+/home/tales/ProjetoPI2/bin/fase1.o: \
+ /home/tales/ProjetoPI2/UsingMake/src/Fases/Fase1/fase1.c \
+ /home/tales/ProjetoPI2/UsingMake/src/Fases/Fase1/fase1.h \
+ /home/tales/ProjetoPI2/UsingMake/src/Fases/Fase1/../../comum.h \
+ /home/tales/ProjetoPI2/UsingMake/src/Fases/Fase1/../../Player.h \
+ /home/tales/ProjetoPI2/UsingMake/src/Fases/Fase1/../../ItensMenu.h
