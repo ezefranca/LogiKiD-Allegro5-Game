9b9b9234205be948a483c257324472207ef6fc70
talespadua <talescpadua@gmail.com>
2013-10-14 21:59:16 -0300

    Adicionado prototipo arquivos de configuração e idioma. Segmentation fault menina.

---
+++ make/src/config.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include "config.h"
+
+struct Config load_config(char *arquivo) {
+
+
+}
+
+struct Idioma load_idioma(char *idioma){
+	char *local_idioma;
+	int loaded;
+	
+	strcpy(local_idioma, "local aqui");
+	strcat(local_idioma, idioma);
+	strcat(local_idioma, ".conf");
+	
+	
+	
+	loaded = 1;
+	while(loaded) {
+		
+		strcpy(local_idioma, "local aqui");
+		strcat(local_idioma, idioma);
+		strcat(local_idioma, ".conf");
+	
+		//Abre idioma
+		FILE *arquivo = fopen(local_idioma, "r");
+	
+		if(file) {
+			
+			
+			
+			loaded = 0;
+		}
+		else {
+			printf("Arquivo de idioma não localizado\n");
+			printf("Will try to load the default language\n");
+			
+		}
+	}
+}
---
+++ make/src/config.h
@@ -0,0 +1,18 @@
+/*
+ * Contem as configurações do sistema
+ * Inicio: 13/10/2013
+ * Autor: Gabriel Fontenelle Senno Silva
+ */
+ 
+typedef struct {
+	char *local_padrao;
+	char *idioma;
+	
+
+} Config;
+
+typedef struct {
+	char *AA00;
+	char *AA01;
+	
+} Idioma;
