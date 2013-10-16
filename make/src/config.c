2a3828f1e477561e574fb9ee4525641832e3dcd9
Gabriel <colecionador.gabriel@gmail.com>
2013-10-15 22:20:33 -0300

    Change functions to handle idioma

--- make/src/config.c
+++ make/src/config.c
@@ -7,36 +7,12 @@ struct Config load_config(char *arquivo) {
 
 }
 
-struct Idioma load_idioma(char *idioma){
-	char *local_idioma;
-	int loaded;
-	
-	strcpy(local_idioma, "local aqui");
-	strcat(local_idioma, idioma);
-	strcat(local_idioma, ".conf");
-	
-	
-	
-	loaded = 1;
-	while(loaded) {
-		
-		strcpy(local_idioma, "local aqui");
-		strcat(local_idioma, idioma);
-		strcat(local_idioma, ".conf");
-	
-		//Abre idioma
-		FILE *arquivo = fopen(local_idioma, "r");
-	
-		if(file) {
-			
-			
-			
-			loaded = 0;
-		}
-		else {
-			printf("Arquivo de idioma não localizado\n");
-			printf("Will try to load the default language\n");
-			
-		}
-	}
+void load_idioma(struct Idioma *idioma, char *key, char *content){
+
 }
+
+void clean_idioma(struct Idioma *idioma) {
+	free(idioma);
+} 
+
+
