c41644094818ccf8a180f2442369842d8f881625
Ezequiel <ezequiel.ifsp@gmail.com>
2013-10-20 22:58:35 -0200

    tabela verdade adicionada na fase KEY_5

--- make/src/StartMenu.c
+++ make/src/StartMenu.c
@@ -11,8 +11,8 @@ void PrintMenu(int MenuPosition)
 	//al_draw_scaled_bitmap(fundo, 0, 0, 640, 480, 0, 0, 800, 600, 0);
 	al_draw_text(game.fonte, al_map_rgb(0, 0, 0), 800 / 2, 302, ALLEGRO_ALIGN_CENTRE, "--------------------------");
 	al_draw_text(game.fonte, al_map_rgb(0, 255, 0), 800 / 2, 305, ALLEGRO_ALIGN_CENTRE, "--------------------------");
-	al_draw_text(game.fonte_logo, al_map_rgb(0, 0, 0), 800 / 2, 350, ALLEGRO_ALIGN_CENTRE, "LOGIC GATES ADVENTURE");
-	al_draw_text(game.fonte_logo, al_map_rgb(0, 255, 0), 800 / 2, 356, ALLEGRO_ALIGN_CENTRE, "LOGIC GATES ADVENTURE");
+	al_draw_text(game.fonte_logo, al_map_rgb(0, 0, 0), 800 / 2, 350, ALLEGRO_ALIGN_CENTRE, "LogiKid");
+	al_draw_text(game.fonte_logo, al_map_rgb(0, 255, 0), 800 / 2, 356, ALLEGRO_ALIGN_CENTRE, "LogiKid");
 	al_draw_text(game.fonte, al_map_rgb(0, 0, 0), 800 / 2, 50, ALLEGRO_ALIGN_CENTRE, "Iniciar jogo");
 	al_draw_text(game.fonte, al_map_rgb(255, 255, 255), 800 / 2, 52, ALLEGRO_ALIGN_CENTRE, "Iniciar jogo");
 	al_draw_text(game.fonte, al_map_rgb(0, 0, 0), 800 / 2, 150, ALLEGRO_ALIGN_CENTRE, "Sair");
